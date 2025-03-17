e6470dbfec441fc72e29dd7f315159cb52e6d765
weideow <owweide@hotmail.com>
2025-03-17 09:21:55 +0800

    fixed app js error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router'; 
 import ClientDashboard from './components/ClientDashBoard';
 import TrainerDashboard from './components/TrainerDashboard';
 import LoginForm from './components/Login';
@@ -8,7 +8,6 @@ import { AuthProvider } from './contexts/authContexts';
 import Register from './components/Register';
 import WorkoutPage from './components/Workouts';
 import ProtectedRoute from './components/Protected';
-
 
 function App() {
   return (
@@ -20,7 +19,8 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard/client" element={<ProtectedRoute><ClientDashboard /></ProtectedRoute>} />
           <Route path="/dashboard/trainer" element={<ProtectedRoute><TrainerDashboard /></ProtectedRoute>} />
-          <Route path="/workouts" element={<WorkoutPage/>}></Route>
+          <Route path="/workouts" element={<ProtectedRoute><WorkoutPage /></ProtectedRoute>} />
+          <Route path="/" element={<LoginForm />} />
         </Routes>
       </Router>
     </AuthProvider>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router'; 
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/Login">Login</Link></li>
-        <li><Link to="/dashboard/client">Client Dashboard</Link></li>
-        <li><Link to="/dashboard/trainer">Trainer Dashboard</Link></li>
-        <li><Link to="/register">Register</Link></li>
+    <nav className="bg-blue-500 p-4">
+      <ul className="flex space-x-4">
+        <li><Link to="/login" className="text-white hover:underline">Login</Link></li>
+        <li><Link to="/dashboard/client" className="text-white hover:underline">Client Dashboard</Link></li>
+        <li><Link to="/dashboard/trainer" className="text-white hover:underline">Trainer Dashboard</Link></li>
+        <li><Link to="/register" className="text-white hover:underline">Register</Link></li>
+        <li><Link to="/workouts" className="text-white hover:underline">Workouts</Link></li>
       </ul>
     </nav>
   );
